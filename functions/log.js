991412df214dada3e3022d6ebb286b5379373cb3
matthewthechickenman <65732060+matthewthechickenman@users.noreply.github.com>
2021-03-06 19:54:57 +0000

    bonk

--- functions/log.js
+++ functions/log.js
@@ -2,7 +2,7 @@ const m = require('../models/index')
 function log (err, id) {
     console.log(`[SHAREX-SERVER] ${err.stack}`)
     if (!id) {
-        const id = process.f.generateID(15, m.err)
+        const id = await process.f.generateID(15, m.err)
         const sch = new m.err({
             _id: id,
             e: err.stack
